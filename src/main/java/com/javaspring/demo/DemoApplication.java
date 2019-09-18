7e3a918f1653ff7d1788cb30d5c468ff93978a21
Anniekobia <kobiaannunziata94@gmail.com>
2019-09-18 17:27:45 +0300

    Post and Update using Feign HTTP Client

--- src/main/java/com/javaspring/demo/DemoApplication.java
+++ src/main/java/com/javaspring/demo/DemoApplication.java
@@ -2,7 +2,9 @@ package com.javaspring.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
--- src/main/java/com/javaspring/demo/spring/TestingRest.java
+++ src/main/java/com/javaspring/demo/spring/TestingRest.java
@@ -1,5 +1,6 @@
 package com.javaspring.demo.spring;
 
+import com.javaspring.demo.FeignRestClient;
 import com.javaspring.demo.spring.model.Movie;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.ParameterizedTypeReference;
@@ -12,19 +13,38 @@ import java.util.List;
 
 @Component
 public class TestingRest implements CommandLineRunner {
+
+    private final FeignRestClient feignRestClient;
+
+    public TestingRest(FeignRestClient feignRestClient) {
+        this.feignRestClient = feignRestClient;
+    }
+
     @Override
     public void run(String... args) throws Exception {
-        RestTemplate restTemplate=new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
 
-        ResponseEntity<List<Movie>> response = restTemplate.exchange("http://10.51.10.111:9090/movies", HttpMethod.GET, null, new ParameterizedTypeReference<List<Movie>>() {});
+        ResponseEntity<List<Movie>> response = restTemplate.exchange("http://10.51.10.111:9090/movies", HttpMethod.GET, null, new ParameterizedTypeReference<List<Movie>>() {
+        });
         List<Movie> movies = response.getBody();
-        System.out.println(movies.toString());
+        System.err.println(movies.toString());
 
-        Movie movie = restTemplate.getForObject("http://10.51.10.111:9090/movies/3",Movie.class);
+        Movie newMovie = new Movie("Why", "2019");
+        newMovie = feignRestClient.createMovie(newMovie);
+        System.out.println("Created Movie: " + newMovie.toString());
+
+        movies = feignRestClient.getAllMovies();
+        System.out.println(movies);
+
+        newMovie.setName("Why are you running");
+        newMovie = feignRestClient.updateMovie(newMovie.getId(),newMovie);
+        System.out.println("Updated Movie: " + newMovie.toString());
+
+        Movie movie = restTemplate.getForObject("http://10.51.10.111:9090/movies/3", Movie.class);
         System.err.println(movie.toString());
 
-        String  url="http://10.51.10.111:9090/movies/search?name="+movie.getName();
-        Movie movieByName = restTemplate.getForObject(url,Movie.class);
+        String url = "http://10.51.10.111:9090/movies/search?name=" + movie.getName();
+        Movie movieByName = restTemplate.getForObject(url, Movie.class);
         System.err.println(movieByName.toString());
     }
 }
